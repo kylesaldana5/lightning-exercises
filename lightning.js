4a737c5e4687267bb5bc39d117314ddc23c42482
kylesaldana5 <kylesaldana5@gmail.com>
2017-12-06 09:34:04 -0600

    12/6 exercise

--- lightning.js
+++ lightning.js
@@ -17,20 +17,24 @@
 // In the console, call.hasOwnProperty() on the new object, passing in an inherited property as an argument.Does it return true or false ?
 //     Do it again, but this time pass in one of the properties you added to the new object.True or false ?  12/5/17
 
-let band = {
-    name: "cool name",
-    president: "trey",
-    products: "jams",
-}
+// let band = {
+//     name: "cool name",
+//     president: "trey",
+//     products: "jams",
+// }
   
-let phish = Object.create(band);
+// let phish = Object.create(band);
     
- phish.keys = "Page";
- phish.bass = "Mike";
+//  phish.keys = "Page";
+//  phish.bass = "Mike";
 
 
-let x = Object.create(phish)
-console.log(x)
+// let x = Object.create(phish)
+// console.log(x)
 
+// Loop over every item in this 
+// array[45, “I”, true, null, “am”, 3.56, “a”, undefined, { catchphrase: “Oh hai, Mark” }, “JS”, “rockstar”]
+// and return a new array that contains only strings.and log out the result. 12/6/17
 
-    
+let array = [45, "I", true, null, "am", 3.56, "a", undefined, { catchphrase: "Oh hai, Mark" }, "JS", "rockstar"].filter((item)=>{return typeof(item) === "string"});
+console.log(array)

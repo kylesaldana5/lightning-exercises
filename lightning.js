ca8642c3dd7a391028c404d1f317e3a99cfc2eb8
kylesaldana5 <kylesaldana5@gmail.com>
2018-01-08 10:14:37 -0600

    1/8/18

--- lightning.js
+++ lightning.js
@@ -97,4 +97,51 @@ for (let i = 0; i < nums.length-1; i++) {
     newNums.push(+nums[i] + +nums[i + 1]);
     
 }
-console.log(newNums)
+// console.log(newNums)
+
+// 1/8/17
+// Loop through this object and add each member object's key as an ID. Log the results.
+let customers = {
+    active: {
+        a87b6345: {
+            name: "Gary Harrison",
+            age: 23,
+            member_level: "gold"
+        },
+        b345hy66: {
+            name: "Harry Garrison",
+            age: 34,
+            member_level: "silver"
+        },
+        nh56yht8: {
+            name: "Mary Terryson",
+            age: 78,
+            member_level: "platinum"
+        },
+        j677yd456: {
+            name: "Barry Sonnyman",
+            age: 44,
+            member_level: "gold"
+        }
+    },
+    inactive: {
+        ur56tfgt: {
+            name: "Barb Justice",
+            age: 56,
+            member_level: "none"
+        },
+        d56gt6: {
+            name: "Jane L'eggo",
+            age: 11,
+            member_level: "none"
+        }
+    }
+}
+
+for(let status in customers){
+    let keys = Object.keys(customers[status])
+    keys.forEach((key) => {
+        customers[status][key].id = key;
+    });
+};
+console.log('',customers );
